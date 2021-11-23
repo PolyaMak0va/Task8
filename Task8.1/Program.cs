d97c749f4599b44c9fb6ce6238f2c58f2deaa302
Ксения Финаева <polya.makova@outlook.com>
2021-11-23 11:17:29 +0300

    Добавьте файлы проекта.

---
+++ Task8.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task8._1
+{
+    class Program
+    {
+        //Программно создайте текстовый файл и запишите в него 10 случайных чисел.
+        //Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, ответ выведите на консоль
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+            string path = @"C:\Users\Ксюша\Desktop\Автоматизация BIM-проектирования\ЗАДАНИЕ 8. РАБОТА С ФАЙЛОВОЙ СИСТЕМОЙ/Logs.txt";
+            int S = 0;
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                int[] array = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    array[i] = random.Next(0, 10);
+                    sw.Write("{0} ", array[i]);
+                    S += array[i];
+                }
+                Console.WriteLine("{0}", S);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Ксюша\Desktop\Автоматизация BIM-проектирования";
+            Console.WriteLine("Выведите на консоль содержимое папки и содержимое всех подкаталогов: ");
+            if (Directory.Exists(path))
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string d in dirs)
+                {
+                    Console.WriteLine(d);
+                    string[] subdir = Directory.GetDirectories(d);
+                    foreach (string s in subdir)
+                    {
+                        Console.WriteLine(s);
+                        string[] files = Directory.GetFiles(s);
+                        foreach (string f in files)
+                        {
+                            Console.WriteLine(f);
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
