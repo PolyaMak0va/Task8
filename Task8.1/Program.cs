6fc2448a516b9174f26a689ba78d0dff86e2f125
Ксения Финаева <polya.makova@outlook.com>
2021-11-26 14:49:03 +0300

    Исправлено 26.11.21

--- Task8.1/Program.cs
+++ Task8.1/Program.cs
@@ -13,12 +13,13 @@ namespace Task8._1
         //Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, ответ выведите на консоль
         static void Main(string[] args)
         {
-            string path = @"C:\Users\Ксюша\Desktop\Автоматизация BIM-проектирования\ЗАДАНИЕ 8. РАБОТА С ФАЙЛОВОЙ СИСТЕМОЙ/Logs.txt";
-
+            string path = @"C:\Users\Ксюша\Desktop\Автоматизация BIM-проектирования\ЗАДАНИЕ 8. РАБОТА С ФАЙЛОВОЙ СИСТЕМОЙ\Logs.txt";
+        
             Random random = new Random();
             if (!File.Exists(path))
             {
-                File.Create(path);
+                var newFile = File.Create(path);
+                newFile.Close();
             }
             using (StreamWriter sw = new StreamWriter(path, false))
             {
@@ -26,7 +27,7 @@ namespace Task8._1
                 for (int i = 0; i < 10; i++)
                 {
                     array[i] = random.Next(0, 10);
-                    sw.Write("{0} ", array[i]);
+                    sw.Write("{0} \n", array[i]);
                 }
             }
             string[] lines = File.ReadAllLines(path);
@@ -37,22 +38,16 @@ namespace Task8._1
             }
             Console.WriteLine("Нажмите любую кнопку для выхода.");
 
-            int S = 0;
-            int N = 0;
-            using (StreamReader sr = new StreamReader(path))
+            int sum = 0;
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
             {
-                Console.WriteLine(sr.ReadToEnd());
-                N = int.Parse(sr.ReadToEnd());
-                //int[] arraySum = new int[lines.Length];
-                //while ((lines = sr.ReadLine()) != null)
-                //    foreach (int l in arraySum)
-                //{
-                //    S += int.TryParse(l);
-                //    S += l;
-                //}          
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    sum += int.Parse(line);
+                }
             }
-            S += N;
-            Console.WriteLine(S);
+            Console.WriteLine(sum);
             Console.ReadKey();
         }
     }
